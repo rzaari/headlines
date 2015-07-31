be1f00b1dfa0b99527cdf3d42b4b4427bedef873
rzaari <ryanzaari@gmail.com>
2015-07-31 15:59:55 -0700

    More API requests added

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -5,12 +5,16 @@ class HeadlinesController < ApplicationController
 	def index
 
 	if Rails.env.production?
-		kimono_url = "https://www.kimonolabs.com/api/daal66ou?apikey=DtN9L9ZLkBTHdqfq50SeqG1JdbHuqpPX"
+		kimono_headlines = "https://www.kimonolabs.com/api/2igf4iz2?apikey=DtN9L9ZLkBTHdqfq50SeqG1JdbHuqpPX"
+		kimono_subheadlines = "https://www.kimonolabs.com/api/dpzfoa9e?apikey=DtN9L9ZLkBTHdqfq50SeqG1JdbHuqpPX"
 	else
-		kimono_url = "http://www.kimonolabs.com/api/daal66ou?apikey=DtN9L9ZLkBTHdqfq50SeqG1JdbHuqpPX"
+		#kimono_url = "http://www.kimonolabs.com/api/daal66ou?apikey=DtN9L9ZLkBTHdqfq50SeqG1JdbHuqpPX"
+		kimono_headlines = "http://www.kimonolabs.com/api/2igf4iz2?apikey=DtN9L9ZLkBTHdqfq50SeqG1JdbHuqpPX"
+		kimono_subheadlines = "http://www.kimonolabs.com/api/dpzfoa9e?apikey=DtN9L9ZLkBTHdqfq50SeqG1JdbHuqpPX"
 	end
 
-	@response =  JSON.parse(RestClient.get(kimono_url))["results"]["collection1"]
+	@headline =  JSON.parse(RestClient.get(kimono_headlines))["results"]["collection1"]
+	@subheadline =  JSON.parse(RestClient.get(kimono_subheadlines))["results"]["collection1"]
 
 	# @json_url = @response["results"]["collection1"][0]["url"]
 	# @json_href = @response["results"]["collection1"][0]["property1"]["href"]
